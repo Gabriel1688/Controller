3c9c2ee9718fefe85081f481752bc53db4b54984
Gabriel1688 <Gabriel1688@users.noreply.github.com>
2024-05-05 16:25:54 -0400

    Update controller.h

--- include/controller.h
+++ include/controller.h
@@ -24,29 +24,7 @@ class Controller
    void Shutdown();
    void Message(const std::string& topic,std::shared_ptr<MESSAGE> message, TCallback callback);
 
-// private:
-    enum class OamAmoCommandType: int
-    {
-       EN_AMO_CMD_GET_AO_REQ = 0,
-       EN_AMO_CMD_CREATE_AO_REQ ,
-       EN_AMO_CMD_DELETE_AO_REQ ,
-       EN_AMO_CMD_UPDATE_AO_REQ ,
-       EN_AMO_CMD_GET_CFM    ,
-       EN_AMO_CMD_ADD_CFM    ,
-       EN_AMO_CMD_ADD_NRCELLCU_CFM,
-       EN_AMO_CMD_SETUP_NRCELLCU_CFM,
-       EN_AMO_CMD_ACTIVE_NRCELLCU_CFM,
-       EN_AMO_CMD_DEACTIVE_NRCELLCU_CFM,
-       EN_AMO_CMD_RELEASE_NRCELLCU_CFM,
-       EN_AMO_CMD_FAULT_INDICATION,
-       EN_AMO_CMD_FAULTCEASE_INDICATION,
-       EN_AMO_CMD_DELETE_CFM ,
-       EN_AMO_CMD_UPDATE_CFM ,
-       EN_AMO_CMD_UPDATE_IND ,
-       EN_AMO_CMD_UNDEFINE
-    };
-
-     void OnMessage(const std::string& topic,std::shared_ptr<MESSAGE> message, TCallback callback);
+   void OnMessage(const std::string& topic,std::shared_ptr<MESSAGE> message, TCallback callback);
      // Common Infrastructure of Active Object.
      static void* EntryOfThread(void* arg);
      void Run();
